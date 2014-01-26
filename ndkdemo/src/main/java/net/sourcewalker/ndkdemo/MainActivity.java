d6595ea70071a2a665a7dee3f2c8e339053216c3
Robert Jacob <xperimental@solidproject.de>
2014-01-26 20:20:32 +0100

    Added views.

--- ndkdemo/src/main/java/net/sourcewalker/ndkdemo/MainActivity.java
+++ ndkdemo/src/main/java/net/sourcewalker/ndkdemo/MainActivity.java
@@ -1,22 +1,38 @@
 package net.sourcewalker.ndkdemo;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Spinner mPrimeLimit;
+    private CheckBox mUseNative;
+    private TextView mResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPrimeLimit = (Spinner) findViewById(R.id.limit);
+        mUseNative = (CheckBox) findViewById(R.id.check_native);
+        findViewById(R.id.start).setOnClickListener(this);
+        mResults = (TextView) findViewById(R.id.results);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.start) {
+            startCalculation();
+        }
+    }
+
+    private void startCalculation() {
+
     }
 
 }
